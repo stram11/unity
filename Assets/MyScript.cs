5eab2a36610d179a82f3b498dfde7ced543d769c
stram11 <108708139+stram11@users.noreply.github.com>
2022-07-05 10:48:43 +0700

    Update MyScript.cs

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -8,6 +8,10 @@ public class MyScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World");
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+        Debug.Log(z);
     }
 
 }
