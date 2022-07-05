8e91e03b733f444c8298baa585e84a59f6c1125d
stram11 <108708139+stram11@users.noreply.github.com>
2022-07-05 10:42:14 +0700

    print hello world

---
+++ Assets/MyScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+   
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+}
