1bc0b5b8abbd5bc70a9ac5e9e752f562ebbc47ff
stram11 <108708139+stram11@users.noreply.github.com>
2022-07-05 15:08:06 +0700

    add Gameplay

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Gameplay : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void OnGui()
+    {
+        GUI.Box(new Rect (100, 100, 200, 50), "Score : ");
+
+
+
     }
 
     // Update is called once per frame
@@ -19,7 +28,15 @@ public class Gameplay : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.gameObject.name.CompareTo("Red Sphere") == 0)
+                if (hit.collider.gameObject.name.CompareTo("RedSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+                if (hit.collider.gameObject.name.CompareTo("GreenSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+                if (hit.collider.gameObject.name.CompareTo("BlueSphere") == 0)
                 {
                     hit.collider.gameObject.SetActive(false);
                 }
